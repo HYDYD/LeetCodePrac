e0f60e1453b2b7fa3afdd67dc1bd91832acaed85
HYDYD_WIN <sdwang3@qq.com>
2019-06-18 19:58:50 +0800

    设计问题 打乱数组 done

---
+++ LeetCode/src/interfaces/ShuffleAnArray.java
@@ -0,0 +1,54 @@
+package interfaces;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by HYDYD.
+ * 2019/6/18 17:53
+ */
+public class ShuffleAnArray {
+    int[] origin, res;
+
+    public ShuffleAnArray(int[] nums) {
+        origin = nums;
+        res = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            res[i] = nums[i];
+        }
+    }
+
+    /**
+     * Resets the array to its original configuration and return it.
+     */
+    public int[] reset() {
+        for (int i = 0; i < origin.length; i++) {
+            res[i] = origin[i];
+        }
+        return res;
+    }
+
+    /**
+     * Returns a random shuffling of the array.
+     */
+    public int[] shuffle() {
+        List<Integer> list = new ArrayList<>();
+        for (int i :
+                res) {
+            list.add(i);
+        }
+        for (int i = 0; i < res.length; i++) {
+            int index = (int) Math.random() * list.size();
+            res[i] = list.remove(index);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,5};
+        ShuffleAnArray s = new ShuffleAnArray(nums);
+        System.out.println(s.reset());
+        System.out.println(s.shuffle());
+    }
+}
